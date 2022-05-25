c09c163f94078841752a926cba480982d4c1292d
CheMetalHead <markocecura@yahoo.com>
2022-05-25 23:05:55 +0200

    pictures

--- components/Featured.jsx
+++ components/Featured.jsx
@@ -23,19 +23,20 @@ function Featured() {
   
   return (
     <div className={styles.container}>
-      <div className={styles.arrowContainer} style={{left:0}} onClick={()=>handleArrow('l')}>
+      featured
+      {/* <div className={styles.arrowContainer} style={{left:0}} onClick={()=>handleArrow('l')}>
         <Image src='/arrowl.png' layout='fill' objectFit='contain'/>
       </div>
       <div className={styles.wrapper} style={{transform:`translateX(${-100*index}vw)`}}>
-          {/* {images.map((img, index)=>(
+          {images.map((img, index)=>(
            <div className={styles.imgContainer} key={index}>
             <Image src={img} layout='fill' objectFit='contain'/>
           </div>
-          ))} */}
+          ))}
       </div>
       <div className={styles.arrowContainer} style={{right:0}} onClick={()=>handleArrow('r')}>
       <Image src='/arrowr.png' layout='fill' objectFit='contain'/>
-      </div>
+      </div> */}
     </div>
   )
 }
